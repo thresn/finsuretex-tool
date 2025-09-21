699132eee4777c32f36228ed03a7c893b47089a2
tahiresin <ttahiresin@gmail.com>
2025-09-21 22:55:46 +0300

    feat:data turned into table form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
 "use client";
 
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
+import csvSplit from "../utils/csvSplit";
+
 
 export default function Page() {
-    const reader = new FileReader();
+    const [rows, setRows] = useState<any[]>([]);
 
 
     function handleFile(e) {
         const file = e.target.files[0]//files arrayınden ilk seçilen file
 
+        const reader = new FileReader();
+
         reader.onload=(ev)=>{//kaydet,okununca buraya dön(onload)
-            const fileText=ev.target.result;
+            const fileText=ev.target.result as string;
+            const data =csvSplit(fileText);
+            setRows(data);
             console.log(fileText);
         };
         
         reader.readAsText(file);
-        
-        
     }
 
     
@@ -25,6 +29,31 @@ export default function Page() {
      <div>
       <input type="file" accept=".csv" onChange={handleFile} />
 
+{rows.length > 0 && (
+
+      <table>
+        <thead>
+         <tr>
+            {Object.keys(rows[0]).map((col) => (
+             <th key={col}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+                            
+        <tbody>
+            {rows.map((row, i) => (
+              <tr key={i}>
+                 {Object.values(row).map((val, j) => (
+                  <td key={j}>{String(val)}</td>
+                  ))}
+               </tr>
+    ))}
+        </tbody>
+
+      </table>
+
+      )}
+      
       
     </div>
 
