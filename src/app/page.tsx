512bed759bf82f6c536a91105a682762e2279507
tahiresin <ttahiresin@gmail.com>
2025-09-23 13:36:46 +0300

    style: format code with Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,96 +5,74 @@ import csvSplit from "../utils/csvSplit";
 import DetailModal from "../components/DetailModal";
 import exportToCSV from "../utils/exportToCsv";
 
-
 export default function Page() {
-    const [rows, setRows] = useState<any[]>([]);
-    const [selectedRow, setSelectedRow] = useState<Record<string, string> | null>(null);//modal için
+  const [rows, setRows] = useState<any[]>([]);
+  const [selectedRow, setSelectedRow] = useState<Record<string, string> | null>(null);//modal için
 
- 
-    function handleFile(e) {
-        const file = e.target.files[0]//files arrayınden ilk seçilen file
+  function handleFile(e) {
+    const file = e.target.files[0]//files arrayınden ilk seçilen file
+    const reader = new FileReader();
 
-        const reader = new FileReader();
+    reader.onload = (ev) => {//kaydet,okununca buraya dön(onload)
+      const fileText = ev.target.result as string;
+      const data = csvSplit(fileText);
+      setRows(data);
+    };
 
-        reader.onload=(ev)=>{//kaydet,okununca buraya dön(onload)
-            const fileText=ev.target.result as string;
-            const data =csvSplit(fileText);
-            setRows(data);
-            console.log(fileText);
-        };
-        
-        reader.readAsText(file);
-    }
+    reader.readAsText(file);
+  }
 
-
-      function handleSave(updatedRow: Record<string, string>) {
-        
-        setRows(
-          rows.map(r => {
-           
-            if (r.id === updatedRow.id) {
-              return updatedRow;   
-            } else {
-              return r;            
-            }
-
-          })
-        );
-
-setSelectedRow(null); // modal kapanır
+  function handleSave(updatedRow: Record<string, string>) {
+    setRows(
+      rows.map(r => {
+        if (r.id === updatedRow.id) {
+          return updatedRow;
+        } else {
+          return r;
         }
+      })
+    );
+    setSelectedRow(null); // modal kapanır
+  }
 
-    
   return (
-
-     <div className="table-wrapper">
+    <div className="table-wrapper">
       <input type="file" accept=".csv" onChange={handleFile} />
+      <button onClick={() => exportToCSV(rows)}>
+        Export CSV
+      </button>
 
-        <button onClick={() => exportToCSV(rows)}>
-          Export CSV
-        </button>
+      {rows.length > 0 && (
+        <table className="table">
 
-
-{rows.length > 0 && (
-
-      <table className="table">
-        <thead className="">
-          
-         <tr>
-            {Object.keys(rows[0]).map((col) => (
-             <th key={col}>{col}</th>
-            ))}
-          </tr>
-        </thead>
-                            
-        <tbody>
-         {rows.map((row) => (
-             <tr key={row.id ?? JSON.stringify(row)} onClick={() => setSelectedRow(row)}>
-                {Object.entries(row).map(([colKey, val]) => (
-                 <td key={colKey}>{String(val)}</td>
-             ))}
+          <thead className="">
+            <tr>
+              {Object.keys(rows[0]).map((col) => (
+                <th key={col}>{col}</th>
+              ))}
             </tr>
+          </thead>
+
+          <tbody>
+            {rows.map((row) => (
+              <tr key={row.id ?? JSON.stringify(row)} onClick={() => setSelectedRow(row)}>
+                {Object.entries(row).map(([colKey, val]) => (
+                  <td key={colKey}>{String(val)}</td>
+                ))}
+              </tr>
             ))}
-        </tbody>
+          </tbody>
 
-
-
-      </table>
+        </table>
 
       )}
-
-      <DetailModal 
-        open={!!selectedRow} 
-        data={selectedRow} 
-        onClose={() => setSelectedRow(null)} 
+      <DetailModal
+        open={!!selectedRow}
+        data={selectedRow}
+        onClose={() => setSelectedRow(null)}
         onSave={handleSave}
       />
-
-      
-      
-      
     </div>
-
 
   )
 }
